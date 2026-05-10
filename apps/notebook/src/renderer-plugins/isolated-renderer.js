242924c8209dd36e1d41e40ed9dbe0d5ae806420
Kyle Kelley <rgbkrk@gmail.com>
2026-05-10 11:01:19 -0700

    build(plugins): bump renderer assets

--- apps/notebook/src/renderer-plugins/isolated-renderer.js
+++ apps/notebook/src/renderer-plugins/isolated-renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c7c223be3904aee44bc2be6e8d02c5202eca2074d46258405568476a338df44
-size 1474951
+oid sha256:d62e026e9e4f3bdddd08dde16b4a50a1e9e4977feb458bd1b342520922e57033
+size 1476789
