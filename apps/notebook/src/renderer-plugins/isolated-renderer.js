2ba1451d00dfe108f584e9b61de526ca2b7612ca
Kyle Kelley <rgbkrk@gmail.com>
2026-05-24 20:23:44 -0700

    chore(release): bump versions for 2.5

--- apps/notebook/src/renderer-plugins/isolated-renderer.js
+++ apps/notebook/src/renderer-plugins/isolated-renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0414b8f29f6db757cc240528f9b1331789d2c186b0d0ee70f553964bf22af7e1
-size 1486178
+oid sha256:984b2c323e90ffc5e7afeb83fa5eac2eaff00d910d120cbb4e7c530d1b3b5d23
+size 1486528
