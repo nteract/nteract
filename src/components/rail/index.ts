cbc19eaa7b8fbba6f6d7c9d1fb1292a752c74ac6
Kyle Kelley <rgbkrk@gmail.com>
2026-06-01 10:41:14 -0700

    refactor(elements): extract generic rail shell (#3293)

---
+++ src/components/rail/index.ts
@@ -0,0 +1,10 @@
+export {
+  Rail,
+  RailButton,
+  RAIL_TAKEOVER_MEDIA_QUERY,
+  RAIL_TAKEOVER_PANEL_CLASS_NAMES,
+  RAIL_TAKEOVER_STAGE_CLASS_NAME,
+  type RailButtonProps,
+  type RailItem,
+  type RailProps,
+} from "./Rail";
