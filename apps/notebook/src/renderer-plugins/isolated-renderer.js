a5d3dbf9fbc22de56f94f9474626b9d32e427ffb
Kyle Kelley <rgbkrk@gmail.com>
2026-06-03 21:31:18 -0700

    build(markdown): update projected renderer bundle

--- apps/notebook/src/renderer-plugins/isolated-renderer.js
+++ apps/notebook/src/renderer-plugins/isolated-renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbda7ea8adadd34d8ff646a76c2b4de1fbeb3d1803f230911bf228a0c220d217
-size 1530354
+oid sha256:026997af5a5dac18575d734fb83661e1a6b8ece1781a9bb47dd149e4eb56dd4c
+size 1531654
