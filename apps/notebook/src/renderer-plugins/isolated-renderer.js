94662fd7ef9eceafeb9dabd67439f8aaf8b85fbf
Kyle Kelley <rgbkrk@gmail.com>
2026-06-04 00:22:32 -0700

    build(markdown): refresh isolated renderer bundle

--- apps/notebook/src/renderer-plugins/isolated-renderer.js
+++ apps/notebook/src/renderer-plugins/isolated-renderer.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:352a4add9099b9c00fc11d212384797c19fbaf3e256916c2bd5c5c6970144ac6
-size 1534134
+oid sha256:8243f6c24e26a47f6f0fefde9f0a0c4e6cb0c168d96376cc411fb9c40bc3ed2f
+size 1534180
